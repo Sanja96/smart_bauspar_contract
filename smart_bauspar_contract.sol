250c0ca9d837a30adce0a9ff8fcd43d7ce6f7592
Sanja96 <wittmannalex3@gmail.com>
2020-05-24 22:05:55 +0200

    Kredit auszahlen

--- smart_bauspar_contract.sol
+++ smart_bauspar_contract.sol
@@ -13,7 +13,19 @@ contract MetaKollektiv {
     address[] Sparphase;
     address[] Kreditphase;
     
-    constructor() public payable {}
+    uint256 Kreditvolumen;
+    
+    struct VertragsDetails {
+        uint256 Sparguthaben;
+        uint256 Kreditvolumen;
+        uint256 Kreditraten;
+    }
+    
+    mapping(address => VertragsDetails) VertragInfo; 
+    
+    constructor() public payable {
+        Kreditvolumen = 0;
+    }
     
     function HinzfuegenAddresse(address payable BSVAdresse,address payable InhaberAdresse) public returns(string memory) {
         /* 
@@ -24,10 +36,15 @@ contract MetaKollektiv {
         BSVZuordnung[BSVAdresse] = InhaberAdresse;
         Vertreage.push(BSVAdresse);
         Sparphase.push(BSVAdresse);
+        VertragInfo[BSVAdresse] = VertragsDetails(0,0,0);
         return "Neuer Bausparvertrag wurde erfolgreich im Kollektiv erfasst!";
     }
     
     function Zugriffsberechtigung(address BSVAdresse) private view returns(bool) {
+        /*
+        Nur Vertreage die bereits gelistet sind, werden für die die jeweilige
+        Transaktion zugelassen.
+        */
         bool gueltig = false;
         for (uint i=0; i<Vertreage.length; i++) {
             if (Vertreage[i] == BSVAdresse) {
@@ -35,6 +52,22 @@ contract MetaKollektiv {
             }
         }
         return gueltig;
+    }
+    
+    function KollektivSparen(address payable BSVAdresse,uint SparrateEingang) external {
+        /*
+        Funktion wird von Bausparvertrag aufgerufen (nach jeder SparTransaktion, um im kollektiv Uberblick zu haben,
+        welcher Vertrag in welchem Umfang gespart hat.
+        */
+        VertragInfo[BSVAdresse] = VertragsDetails(VertragInfo[BSVAdresse].Sparguthaben + SparrateEingang,0,0);
+    }
+    
+    function kollektivTilgen(address payable BSVAdresse,uint KreditVolumen,uint KreditRaten) external payable {
+        /*
+        Funktion wird von Bausparvertrag aufgerufen (nach jeder Kredit/ Tilgen-Transaktion, 
+        um im kollektiv Uberblick zu haben, welcher Vertrag in welchem Umfang getilgt hat hat.
+        */
+        VertragInfo[BSVAdresse] = VertragsDetails(0,KreditVolumen,VertragInfo[BSVAdresse].Kreditraten + KreditRaten);
     }
 
     function GuthabenAuszahlen(address payable BSVAdresse,uint Auszahlung) external payable {
@@ -58,15 +91,35 @@ contract MetaKollektiv {
     }
     
     function KreditAuszahlen(address payable BSVAdresse,uint Auszahlung) external payable returns(string memory) {
+        /*
+        Pruefen ob der Vertrag auch zu einer Kreditauszahlung berechtigt ist
+        und ob genügend Liquiditiät im Kollektiv vorhanden ist.
+        Erst dann erfolgt eine Auszahlung.
+        */
         require(Zugriffsberechtigung(BSVAdresse) == true,"Keine Berechtigung zum Zugriff");
         Bausparvertrag bsv = Bausparvertrag(BSVAdresse);
+        require(bsv.ZuteilungsReife()==true);
         require(address(this).balance > 0,"Nicht ausreichend Liquiditaet im kollektiv");
         BSVZuordnung[address(bsv)].transfer(Auszahlung);
+        /* 
+        Umschluesselung von Sparphase auf Kreditphase
+        nach erfolgreicher Auszahlung.
+        */
+        Kreditvolumen += Auszahlung;
+        
+        bool loop = false;
+        uint i = 0;
+        while(!loop) {
+            if (Sparphase[i] == BSVAdresse) {
+                Kreditphase.push(Sparphase[i]);
+                delete Sparphase[i];
+                loop = true;
+            } else i++;
+        }
     }
     
     function getMetadata() public view returns(uint,uint,uint,uint,uint) {
-        //ToDo: Anzahl an Krediten im Kollektiv und ausgezahltes Volumen
-        return (Vertreage.length,Sparphase.length,Kreditphase.length,address(this).balance,0);
+        return (Vertreage.length,Sparphase.length,Kreditphase.length,address(this).balance,Kreditvolumen);
     }
     
     function getVertrag(address BSVAdresse) public view returns(address) {
@@ -82,7 +135,6 @@ contract MetaKollektiv {
 contract Bausparvertrag {
     //Alle wichtigen e Adressen
     address payable owner;
-    //address payable _kollektiv = 0xdD870fA1b7C4700F2BD7f44238821C26f7392148;
     address payable _metakollektiv = 0x3643b7a9F6338115159a4D3a2cc678C99aD657aa;
 
     //Informationen über Vertrags-InhaberIn
@@ -131,7 +183,7 @@ contract Bausparvertrag {
         owner = msg.sender;
         AnzahlInhaber += 1;
         Inhaber[AnzahlInhaber] = Person(AnzahlInhaber,_firstname,_lastname,owner,bsv_address);
-        BausparSumme = _summe * 10^18;
+        BausparSumme = _summe * 10**18;
         Guthaben = 0 - (int(BausparSumme) / 100); //Abschlussgebuehr zahlen 1.0%
         ZahlungsHistorie[NumZahlung] = Details(0,VertragsAbschluss,'Vertragsabschluss');
         /*Meta Kollektiv Reporting*/
@@ -156,6 +208,7 @@ contract Bausparvertrag {
         */
         if (keccak256(bytes(Phase)) == keccak256(bytes('Sparphase'))) {
             address(k).transfer(msg.value);
+            k.KollektivSparen(bsv_address,msg.value);
             Guthaben += (int256) (msg.value);
             SparEingang = block.timestamp;
             NumZahlung += 1;
@@ -185,7 +238,7 @@ contract Bausparvertrag {
         }
     }
     
-    function ZuteilungsReife() public view returns(bool) { 
+    function ZuteilungsReife() public nurInhaber view returns(bool) { 
         /*
         Berechnung der Kennzahlen zur Ermittlung der ZuteilungsReife
         des Vertrags. Hierzu wird ermittelt, wie viel Prozent von der BausparSumme
@@ -194,21 +247,40 @@ contract Bausparvertrag {
         Leifert ZuteilungsReife ein true, so ist der Vertrag für eine Auszahlung eines Kreditphase
         aus dem Kollektiv berechtigt.
         */
-        int Eingezahlt;
+        int Eingezahlt = 0;
         uint DauerSparphase = ZahlungsHistorie[NumZahlung]._Zeitpunkt - ZahlungsHistorie[0]._Zeitpunkt;
         if (keccak256(bytes(Phase)) == keccak256(bytes('Sparphase')) && Guthaben > 0) {
-            Eingezahlt = (Guthaben / int(BausparSumme)) * 100;
-            if (Eingezahlt >= 45 && DauerSparphase >= 100) {
+            Eingezahlt = (Guthaben * 100/ int(BausparSumme));
+            if (Eingezahlt >= 45 && DauerSparphase >= uint(10)) {
                 return true;
             } else return false;
         } else return false;
     }
     
-    function KreditAntrag() public nurInhaber returns(string memory) {
+    function KreditAntrag() public payable nurInhaber returns(string memory) {
+        /*
+        Sobald ZuteilungsReife erfüllt ist, kann das restliche Volumen aus dem 
+        Bausparvertrag als Kredit beantragt werden. (Bausparsumme abzgl. des bisher gesparten
+        Guthabens). Guthaben und Kreditvolumen werden folglich aus dem Kollektiv Vertrag ausgezahlt.
+        */
         if (ZuteilungsReife() == true) {
             Phase = 'Kreditphase';
             Kreditsumme = int(BausparSumme) - Guthaben;
+            k.KreditAuszahlen(bsv_address,uint(Kreditsumme + Guthaben));
+            Guthaben = 0;
+            k.kollektivTilgen(bsv_address,uint(Kreditsumme),0);
+            
+            return "Auszahlung Guthaben inkl. Kredit erfolgreich durchgeführt.";
         }
+    }
+    
+    function KreditTilgen() public payable nurInhaber {
+        require(keccak256(bytes(Phase)) == keccak256(bytes('Kreditphase')),"Tigen nur in der Kreditphase möglich");
+        
+        address(k).transfer(msg.value);
+        Kreditsumme -= Kreditsumme - int(msg.value);
+        k.kollektivTilgen(bsv_address,uint(Kreditsumme),msg.value);
+        
     }
     
     /*Getter Funktionen vom Vertrag definiert*/
@@ -219,6 +291,10 @@ contract Bausparvertrag {
     
     function getKollektiv() public view nurInhaber returns(uint256){
         return address(k).balance;
+    }
+    
+    function getEingezahlt() public view returns(int,int,int){
+        return (Guthaben,int(BausparSumme),(Guthaben * 100 / int(BausparSumme)));
     }
     
     /*Fallback und Receiver Funktionen*/
